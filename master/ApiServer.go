6cfe4eba1f4d01eba04d62ece411a672da4a6f8a
吴问应 <wenying.wu@baisonmail.com>
2020-05-30 19:45:37 +0800

    强杀

--- master/ApiServer.go
+++ master/ApiServer.go
@@ -104,6 +104,26 @@ ERR:
 
 //强杀任务
 func handleJobKill(resp http.ResponseWriter, req *http.Request) {
+	var (
+		err   error
+		name  string
+		bytes []byte
+	)
+	if err = req.ParseForm(); err != nil {
+		goto ERR
+	}
+	name = req.PostForm.Get("name")
+	if err = G_jobMgr.KillJob(name); err != nil {
+		return
+	}
+	if bytes, err = common.BuildResponse(0, "success", nil); err == nil {
+		resp.Write(bytes)
+	}
+	return
+ERR:
+	if bytes, err = common.BuildResponse(-1, err.Error(), nil); err == nil {
+		resp.Write(bytes)
+	}
 
 }
 
